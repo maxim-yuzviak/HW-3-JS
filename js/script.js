b9491f70761064226508f34e91f3a347718c502f
Maxim Yuzviak <shaylushaytop@gmail.com>
2025-03-17 19:11:13 +0100

    done all

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let result = 5 + 5 + '5';
+console.log(result)
+console.log(typeof result)
+let email = 'shaylushaytop@gmail.com'
+console.log(email.length)
+email.includes("@")
+console.log(email.includes("@"))
+let My = "My ";
+let Name = "name ";
+let Is = "is ";
+let fullName = My + Name + Is + 'Viktor';
+console.log(fullName)
+let userName = prompt("Як вас звати?");
+alert(`Дякуємо ${userName},  До сплати 300 гривень`)
